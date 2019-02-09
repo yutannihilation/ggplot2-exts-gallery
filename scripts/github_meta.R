49a543e6d9ae5b5a27824a63a0dd3f40d7d8eb51
Hiroaki Yutani <yutani.ini@gmail.com>
2019-02-09 13:59:26 +0900

    Use gh and update github_meta.json

--- scripts/github_meta.R
+++ scripts/github_meta.R
@@ -4,20 +4,19 @@
 
 suppressMessages(library(yaml))
 suppressMessages(library(jsonlite))
-suppressMessages(library(github))
+suppressMessages(library(gh))
 
 message("* setting up context")
-ctx <- create.github.context(personal_token = Sys.getenv("GITHUB_TOKEN"))
 
 message("* loading yaml")
 yml <- yaml.load_file("_config.yml")
 
 meta <- lapply(yml$widgets, function(wdgt) {
   message("*** getting meta data for: ", wdgt$ghuser, " ", wdgt$ghrepo)
-  a <- try(get.repository(wdgt$ghuser, wdgt$ghrepo))
-  if(inherits(a, "try-error"))
+  res <- try(gh("GET /repos/:owner/:repo", owner = wdgt$ghuser, repo = wdgt$ghrepo))
+  if(inherits(res, "try-error"))
     return(list(stargazers_count = 0))
-  a$content[c("stargazers_count", "open_issues_count", "forks_count", "watchers_count")]
+  res[c("stargazers_count", "open_issues_count", "forks_count", "watchers_count")]
 })
 
 names(meta) <- sapply(yml$widgets, function(x) paste(x$ghuser, x$ghrepo, sep = "_"))
@@ -26,7 +25,7 @@ all_good <- sapply(meta, function(x) is.numeric(x$stargazers_count))
 
 if(all(all_good) && length(yml$widgets) == length(meta)) {
   message("* saving results")
-  cat(toJSON(meta, auto_unbox = TRUE), file = "github_meta.json")
+  cat(toJSON(meta, auto_unbox = TRUE, pretty = TRUE), file = "github_meta.json")
 } else {
   cat("ERROR - NOT UPDATING REPO")
 }
